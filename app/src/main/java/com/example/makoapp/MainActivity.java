dc1d7eb86a51e022509181d6a05d00dac9a3eb50
AdaNamanya0 <namanyaaada@gmail.com>
2020-02-07 12:31:00 +0300

    Initial Commit

--- app/src/main/java/com/example/makoapp/MainActivity.java
+++ app/src/main/java/com/example/makoapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.makoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,12 +15,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
     public void sendMessage (View view) {
-        EditText message = (EditText)findViewById(R.id.message);
+        TextView message = findViewById(R.id.message);
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         intent.putExtra("MESSAGE", message.getText().toString());
         startActivity(intent);
         message.setText("");
+
     }
 
 }

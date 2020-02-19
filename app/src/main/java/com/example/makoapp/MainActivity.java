3fba87de7ae567501d27c459b362797fe169eaa0
AdaNamanya0 <namanyaaada@gmail.com>
2020-02-19 17:22:07 +0300

    wed 19/02/2020

--- app/src/main/java/com/example/makoapp/MainActivity.java
+++ app/src/main/java/com/example/makoapp/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,6 +18,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.menu, menu);
+
+        return true;
+
+    }
+
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menu_email) {
+            Intent myintent = new Intent(this, email.class);
+            this.startActivity(myintent);
+            return true;
+        }
+
+        if (id == R.id.menu_food) {
+            Intent myintent = new Intent(this, food.class);
+            this.startActivity(myintent);
+            return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
 
 
     public void sendMessage (View view) {
